a33ba8534574cea6083d0717251f959ece094045
keiya01 <keiya.s.0210@gmail.com>
2019-12-14 13:23:45 +0900

    Initialized

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+module.exports = {
+	entry: "./src/index.ts",
+	devtool: "inline-source-map",
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: "ts-loader",
+				exclude: /node_modules/
+			}
+		]
+	},
+	resolve: {
+		extensions: [".js", ".jsx", ".ts", ".tsx"]
+	},
+	output: {
+		filename: "bundle.js",
+		path: path.resolve(__dirname, "public")
+	},
+	devServer: {
+		contentBase: path.join(__dirname, "public"),
+		inline: true,
+		compress: true,
+		port: 9000
+	}
+};
